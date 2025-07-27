ba6fc79fca6ce3bc5f81f35b8cde7029105f151c
Najmul Huda <nh5696080@gmail.com>
2025-07-27 12:48:45 +0300

    Update javaScript.js

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -1,18 +1,15 @@
- const display = document.getElementById('display');
-
-        function appendToDisplay(value) {
-            display.value += value;
-        }
-
-        function clearDisplay() {
-            display.value = '';
-        }
-
-        function calculate() {
-            try {
-                display.value = eval(display.value) || '';
-            } catch (error) {
-                display.value = 'Error';
-                setTimeout(clearDisplay, 1000);
-            }
-        }
+const display = document.getElementById("display");
+function appendToDisplay(value) {
+  display.value += value;
+}
+function clearDisplay() {
+  display.value = "";
+}
+function calculate() {
+  try {
+    display.value = eval(display.value) || "0";
+  } catch (error) {
+    display.value = "error";
+    setTimeout(clearDisplay, 1000);
+  }
+}
